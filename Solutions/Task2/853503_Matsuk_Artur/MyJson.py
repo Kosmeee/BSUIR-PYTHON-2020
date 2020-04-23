07f640499bcc9886dd8365b1cd3bf47be4667bfb
Kosmeee <44471504+Kosmeee@users.noreply.github.com>
2020-04-23 17:42:12 +0300

    Add files via upload

---
+++ Solutions/Task2/853503_Matsuk_Artur/MyJson.py
@@ -0,0 +1,52 @@
+def list_or_tuple_tojson(obj):
+    ret = '['
+    for i in obj[:-1]:
+        ret += indict_to_json(i) + ', '
+    ret += indict_to_json(obj[-1])
+    ret += ']'
+    return ret
+
+
+def indict_to_json(obj: dict):
+    ret = ""
+
+    if isinstance(obj, dict):
+        ret = into_json(obj)
+
+    elif isinstance(obj, list) or isinstance(obj, tuple):
+        ret = list_or_tuple_tojson(obj)
+
+    elif isinstance(obj, bool):
+        if obj:
+            ret = "true"
+        else:
+            ret = "false"
+
+    elif isinstance(obj, int) or isinstance(obj, float) or isinstance(obj, complex):
+        ret = str(obj)
+
+    elif isinstance(obj, str):
+        ret = '"{}"'.format(str(obj))
+
+    elif obj is None:
+        ret = 'null'
+
+    return ret
+
+
+def into_json(obj: object):
+    json = "{"
+    if isinstance(obj, dict):
+        dict1 = obj
+    for key, value in list(dict1.items()):
+        if key != list(dict1.keys())[-1]:
+            json += '"{}": {}'.format(str(key), indict_to_json(value)) + ', '
+        else:
+            json += '"{}": {}'.format(str(key), indict_to_json(value))
+
+    json += "}"
+    return json
+
+
+def to_json(obj: object):
+    return into_json(obj)
---
+++ Solutions/Task2/853503_Matsuk_Artur/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Artur_Matsuk_lab2',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Kosmeee',
+    license='',
+    author='Artur Matsuk',
+    author_email='lall@tut.by',
+    description='lab №2'
+)
